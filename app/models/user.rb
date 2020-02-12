1074b71bdb671fd697c80d77f73f6bb0e182de8c
ookidoshi <okidookido@gmail.com>
2020-02-12 18:08:57 +0900

    like_fin

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
 
 
   has_many :posts, dependent: :destroy
+  has_many :likes
 
 
 
-  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
