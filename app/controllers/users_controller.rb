fb93467692681a886e69c7f28a1a8573cd70c31b
ookidoshi <okidookido@gmail.com>
2020-02-18 11:05:38 +0900

    show_fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find_by(id: params[:id])
+    @photos = Photo.all
+    # @photos = Photo.includes(:image).order("created_at DESC").limit(10)
   end
 
 end
