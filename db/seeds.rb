518369ec7a8f04a17464000c61707f016f58165b
ookidoshi <okidookido@gmail.com>
2020-02-25 17:48:21 +0900

    test_user
    test_user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+user = User.create!(
+  email:     "sample@gmail.com",
+  name:      "test",
+  password:  "test567",
+)
