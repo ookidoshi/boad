7e4bff6bb17cc424177b4d6abb9464fe2b5e26c7
ookidoshi <okidookido@gmail.com>
2020-03-16 16:03:56 +0900

    initial commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-    @photos = Photo.all
-    # @photos = Photo.includes(:image).order("created_at DESC").limit(10)
+    
+    # @photos = Photo.includes(:images).order("created_at DESC").limit(10)
+    @photos = Photo.order("created_at DESC").limit(4)
   end
 
 end
